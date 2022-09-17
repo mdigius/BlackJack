290a5cad5b9b7a51742526a3320b8cca8a4d8d39
mdigius <mdigius@uwo.ca>
2022-09-17 11:29:12 -0400

    Added player class and improved demoi

--- BlackJack/src/Demo.java
+++ BlackJack/src/Demo.java
@@ -31,26 +31,71 @@ class Demo {
         int handSum = 0;
         int cash = 1000;
         int bet = 0;
+        int turns = 5;
 
-        System.out.print("Welcome to BlackJack! Your starting cash is $1000.\nPlease enter the amount" +
-                " you would like to bet: ");
-        bet = scan.nextInt();
+        Player player = new Player(1000);
+        Player dealer = new Player(0);
 
-        for(int i=0; i<2; i++){
-            int holder = 0;
-            holder = random.nextInt(0,deckLength);
-            System.out.println(deckID.get(holder));
-            handSum = handSum + deckID.get(holder).getValue();
-            deckID.remove(holder);
-            deckLength = deckLength - 1;
+        System.out.println("Welcome to BlackJack!");
+
+        while(turns>0 && player.getCash()>0){
+            System.out.print("Current cash: $" + player.getCash()+ ".\nPlease enter the amount you would like to bet: ");
+            player.setBet(scan.nextInt());
+            while(player.betChecker()==false){
+                System.out.print("Bet exceeds cash. Enter a new amount you would like to bet: ");
+                player.setBet(scan.nextInt());
+
+            }
+            for(int i=0; i<2; i++){
+                int holder = 0;
+                holder = random.nextInt(0,deckLength);
+                System.out.println(deckID.get(holder));
+                player.setSum(player.getSum()+deckID.get(holder).getValue());
+                deckID.remove(holder);
+                deckLength = deckLength - 1;
+            }
+            while(dealer.getSum()<15){
+                int holder = 0;
+                holder = random.nextInt(0,deckLength);
+                dealer.setSum(dealer.getSum()+deckID.get(holder).getValue());
+                deckID.remove(holder);
+                deckLength = deckLength - 1;
+            }
+            System.out.println("Current Hand: " + player.getSum());
+            System.out.print("Please type H to hit, or S to stand: ");
+            String response;
+            response = scan.next();
+            while(response.equalsIgnoreCase("h" )){
+                for(int i=0; i<1; i++){
+                    int holder = 0;
+                    holder = random.nextInt(0,deckLength);
+                    System.out.println(deckID.get(holder));
+                    player.setSum(player.getSum()+deckID.get(holder).getValue());
+                    deckID.remove(holder);
+                    deckLength = deckLength - 1;
+                }
+                System.out.print("Please type H to hit, or S to stand: ");
+                response = scan.next();
+            }
+            System.out.println("Your sum: "+ player.getSum());
+            System.out.println("Dealer sum: " + dealer.getSum());
+            if(player.getSum()> dealer.getSum()){
+                player.betSuccess();
+                System.out.println("You win!");
+            } else{
+                System.out.println("You suck cock");
+            }
+            turns--;
+            player.setSum(0);
+            dealer.setSum(0);
+
+
+
+
         }
-        System.out.println("Current Hand: " + handSum);
-        System.out.print("Please type H to hit, or S to stand: ");
-        String response = scan.next();
 
-        if(response.equalsIgnoreCase("h")){
 
         }
     }
-}
+
 
